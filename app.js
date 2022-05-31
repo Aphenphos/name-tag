1dea47559fa9c4e8c3d4216a2a1c18e624402d99
Aphenphos <MaxwillWinters@gmail.com>
2022-05-31 19:04:26 -0400

    tested all event listeners and made age only accept number inputs

--- app.js
+++ app.js
@@ -4,13 +4,19 @@ const ageInput = document.getElementById('age-input');
 
 nameInput.addEventListener('input', () => {
     console.log('input happened!');
+    const name = nameInput.value;
+    console.log(name);
 });
 
 roleInput.addEventListener('input', () => {
     console.log('input happened!');
+    const role = roleInput.value;
+    console.log(role);
 });
 
 ageInput.addEventListener('input', () => {
     console.log('input happened!');
+    const age = ageInput.value;
+    console.log(age);
 });
 
