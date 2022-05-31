de17e7f4f523bf13d5aa4525bdde1d16bf2de5ef
Aphenphos <MaxwillWinters@gmail.com>
2022-05-31 18:01:26 -0400

    added name display

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 //grabs name
 const nameInput = document.getElementByID('name-input');
+const nameDisplay = document.getElementById('name-display');
+console.log(name-display);
+
 console.log(nameInput);
 
-nameInput.addEventListener('name-input' () => {
-    
+nameInput.addEventListener('name-input', () => {
+    const name = nameInput.value;
+    nameDisplay.textContent = name;
     console.log('input happened');
+    console.log(name);
 });
+
