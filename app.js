265db7ef98e5667b593a20788ae4af25fececec3
Aphenphos <MaxwillWinters@gmail.com>
2022-05-31 18:57:25 -0400

    console properly logging the inputs i thin?

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
-// grab DOM elements
+const nameInput = document.getElementById('name-input');
+const roleInput = document.getElementById('role-input');
+const ageInput = document.getElementById('age-input');
 
-// set event listeners
-    // get info from user input
-    // use user input to update state
-    // update DOM to reflect the new state
+console.log(nameInput);
+console.log(roleInput);
+console.log(ageInput);
